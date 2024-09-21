e847aaaad42681d496d55ba376605e1d810ed877
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2024-09-21 19:45:34 +0000

    conf

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -4,30 +4,18 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed base-devel --noconfirm
 sudo pacman -S yay --needed --noconfirm
 sudo pacman -S --noconfirm --needed - < packages/pckfull.txt 2>/dev/null
-# yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
-#
-#
-#
+yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
+
 ### Replace PROGRAM_NAME with the actual program you want to check
-PROGRAM_NAME="hyprland" 
-file="$HOME/.config/hyprlan"                                                             
-if [ -f "$file" ]; then                                                                          
-  echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
-# II
-fileh="$HOME/.config/hyprland"
-if [ ! -d "$fileh" ]; then
-  echo "Hyprland is already installed."
-else
-  echo "Hyprland is not installed. Proceeding with the installation..."
-  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
-fi
-## III
-filez="$HOME/.oh-my-zsh/oh-my-zsh.sh"
-if [ ! -f "$filez" ]; then
-  echo "Oh My Zsh is already installed."
-else
-  echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-
+#PROGRAM_NAME="hyprland" 
+#file="$HOME/.config/hyprlan"                                                             
+#if [ -f "$file" ]; then                                                                          
+#  echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
+## II
+#fileh="$HOME/.config/hyprland"
+#if [ ! -d "$fileh" ]; then
+#  echo "Hyprland is already installed."
+#else
+#  echo "Hyprland is not installed. Proceeding with the installation..."
+#  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
+#fi
