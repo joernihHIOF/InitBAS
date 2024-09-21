1853b0d3d4150ec548ed67a19ebd0a097610639a
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2024-09-21 19:11:12 +0000

    pagu

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -4,13 +4,12 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed base-devel --noconfirm
 sudo pacman -S yay --needed --noconfirm
 sudo pacman -S --noconfirm --needed - < packages/pckfull.txt 2>/dev/null
-yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
-
-
-
-## Replace PROGRAM_NAME with the actual program you want to check
+# yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
+#
+#
+#
+### Replace PROGRAM_NAME with the actual program you want to check
 PROGRAM_NAME="hyprland" 
-
 file="$HOME/.config/hyprlan"                                                             
 if [ -f "$file" ]; then                                                                          
   echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
@@ -22,7 +21,7 @@ else
   echo "Hyprland is not installed. Proceeding with the installation..."
   bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
 fi
-# III
+## III
 filez="$HOME/.oh-my-zsh/oh-my-zsh.sh"
 if [ ! -f "$filez" ]; then
   echo "Oh My Zsh is already installed."
