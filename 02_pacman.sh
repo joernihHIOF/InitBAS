33c35dd59ba968cf395d6abfec1b84deede76584
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2024-10-26 18:40:21 +0200

    abc

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm
-sudo pacman -S --needed base-devel --noconfirm
-sudo pacman -S yay --needed --noconfirm
+# sudo pacman -Syu --noconfirm
+# sudo pacman -S --needed base-devel --noconfirm
+# sudo pacman -S yay --needed --noconfirm
 sudo pacman -S --noconfirm --needed - < packages/pckfull.txt 2>/dev/null
 yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
 
