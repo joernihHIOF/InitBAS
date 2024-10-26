666d57c16b0891e393014d38bcd749a888ded3f0
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2024-10-26 18:44:31 +0200

    improved

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -3,8 +3,8 @@
 # sudo pacman -Syu --noconfirm
 # sudo pacman -S --needed base-devel --noconfirm
 # sudo pacman -S yay --needed --noconfirm
-sudo pacman -S --noconfirm --needed - < packages/pckfull.txt 2>/dev/null
-yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
+sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.txt 2>/dev/null
+yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
 
 if [ ! -d "$fileh" ]; then
   echo "Hyprland is already installed."
