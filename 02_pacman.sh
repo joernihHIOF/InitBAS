3e6c40477717c2a05ab8a676ef2e9245ec9ba6a3
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2024-10-26 18:38:06 +0200

    abc

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -7,8 +7,8 @@ sudo pacman -S --noconfirm --needed - < packages/pckfull.txt 2>/dev/null
 yay         -S --noconfirm --needed - < packages/yayfull.txt 2>/dev/null
 
 if [ ! -d "$fileh" ]; then
+  echo "Hyprland is already installed."
+else
   echo "Hyprland is not installed. Proceeding with the installation..."
   bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
-else
-  echo "Hyprland is already installed."
 fi
