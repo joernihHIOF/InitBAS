db8257d4e3f9467eb3fc7c429bec850b90e5d269
joernihHIOF <jorn.i.halvorsen@hiof.no>
2024-10-27 09:31:15 +0100

    updR

--- 02_pacman.sh
+++ 02_pacman.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-sudo pacman -Syu --noconfirm
-sudo pacman -S --needed base-devel --noconfirm
-sudo pacman -S yay --needed --noconfirm
+#sudo pacman -Syu --noconfirm
+#sudo pacman -S --needed base-devel --noconfirm
+#sudo pacman -S yay --needed --noconfirm
 sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.txt 2>/dev/null
 yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
-
-if [ ! -d "$fileh" ]; then
-  echo "Hyprland is already installed."
-else
-  echo "Hyprland is not installed. Proceeding with the installation..."
-  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
-fi
+#
+#if [ ! -d "$fileh" ]; then
+#  echo "Hyprland is already installed."
+#else
+#  echo "Hyprland is not installed. Proceeding with the installation..."
+#  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
+#fi
