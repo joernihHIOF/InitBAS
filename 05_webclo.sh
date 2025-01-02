48dd9c397c64f5a4ae305037e3f572c4f64b1328
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-01-02 22:42:00 +0100

    wp2

--- 05_webclo.sh
+++ 05_webclo.sh
@@ -6,13 +6,13 @@ clone_if_not_exists() {
     target_dir="$2"
     
     if [ ! -d "$target_dir" ]; then
-        git clone "$repo_url" "$target_dir"
+        git clone --depth 1 "$repo_url" "$target_dir"
     else
         echo "Directory $target_dir already exists. Skipping clone."
     fi
 }
 
-# git clone --depth 1 https://github.com/makccr/wallpapers "$HOME/.config/wallpapers/*"
+clone_if_not_exists "https://github.com/makccr/wallpapers" "$HOME/.config/wallpapers2/*"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
--- README.md
+++ README.md
@@ -28,5 +28,3 @@
 # alias.sh
 # functions.sh
 
-
-
