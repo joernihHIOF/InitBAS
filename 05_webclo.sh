eac2557f5ab2b1c13da91daa83dd5c3c1517f72a
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-01-02 22:20:19 +0100

    abc

--- 05_webclo.sh
+++ 05_webclo.sh
@@ -12,7 +12,7 @@ clone_if_not_exists() {
     fi
 }
 
-git clone --depth 1 https://github.com/makccr/wallpapers "$HOME/.config/wallpapers/*"
+# git clone --depth 1 https://github.com/makccr/wallpapers "$HOME/.config/wallpapers/*"
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
--- config/hyprland/reload-hyprpaper.sh
+++ config/hyprland/reload-hyprpaper.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Old
 killall hyprpaper
 sleep 1
 hyprpaper &
+
+# New
+#WALLPAPER_DIR="$HOME/.config/wallpapers/*"
+#WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+#MONITOR=$(hyprctl monitors | grep "Monitor" | cut -d " " -f 2)
+#
+#hyprctl hyprpaper unload all
+#hyprctl hyprpaper preload "$WALLPAPER"
+#hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
