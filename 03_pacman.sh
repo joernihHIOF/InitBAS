b77f76813154c1d807d32ae7e5fe93699698a5a4
joernihHIOF <jorn.halvorsen@hifo.no>
2025-01-03 12:03:47 +0100

    pacmanstart

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+echo 'pacman starter'
 sudo pacman -Sy archlinux-keyring manjaro-keyring --needed
 sudo pacman -S --needed base-devel --noconfirm
-sudo pacman -Syu --noconfirm
 sudo pacman -S yay --needed --noconfirm
-yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
 sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.txt 2>/dev/null
-## I
+yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
+### I
 fileh="$HOME/.config/hypr"                                                             
 if [ ! -d "$fileh" ]; then
-  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
   echo "Hyprland is not installed. Proceeding with the installation..."
+  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
 else
   echo "Hyprland is already installed."
 ## II
