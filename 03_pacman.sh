2e584a8ab5d8c4352b3720a9e97d1774625eb1b4
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2025-04-18 16:37:24 +0200

    abc

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -7,30 +7,14 @@ sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.t
 yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
 
 ### I
-#fileh="$HOME/.config/hypr"                                                             
-#if [ ! -d "$fileh" ]; then
-#  echo "Hyprland is not installed. Proceeding with the installation..."
-#  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
-#else
-#  echo "Hyprland is already installed."
+fileh="$HOME/.config/hypr"                                                             
+if [ ! -d "$fileh" ]; then
+  echo "Hyprland is not installed. Proceeding with the installation..."
+  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
+else
+  echo "Hyprland is already installed."
+fi
 
-### II
-#echo 'oh-my-zsh'
-#fi
-#filez="$HOME/.oh-my-zsh/oh-my-zsh.sh"
-#if [ ! -f "$filez" ]; then
-#  echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
-#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#else
-#  echo "Oh My Zsh is already installed."
-#fi
-#
-## Function to check if a package is installed
-#is_installed() {
-#    pacman -Qi "$1" &> /dev/null
-#    return $?
-#}
-#
 ## Check if Yay is already installed
 #if is_installed "yay"; then
 #    echo "Yay is already installed."
@@ -54,6 +38,24 @@ yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.t
 #    else
 #        echo "Yay installation failed."
 #    fi
-# fi
+#fi
 #
+### II
+echo 'oh-my-zsh'
+filez="$HOME/.oh-my-zsh/oh-my-zsh.sh"
+if [ ! -f "$filez" ]; then
+  echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed."
+fi
+#
+## Function to check if a package is installed
+#is_installed() {
+#    pacman -Qi "$1" &> /dev/null
+#    return $?
+#}
+
+
+
 
