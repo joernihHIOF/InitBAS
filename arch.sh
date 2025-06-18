439cb84412734e6e340335d00ee82d814e7485a5
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-06-18 21:42:26 +0200

    upd

--- arch.sh
+++ arch.sh
@@ -2,5 +2,10 @@ wget https://mirror.archlinux.no/iso/2025.06.01/archlinux-2025.06.01-x86_64.iso
 sudo dd if=archlinux-2025.06.01-x86_64.iso of=/dev/sda bs=4M status=progress oflag=sync
 sync
 sudo eject /dev/sdX
-
-
+iwctl
+device list
+device <device> set-property Powered on
+station <device> scan 
+station <device> get-networks
+station <device> connect <ssid>
+station <device> show 
