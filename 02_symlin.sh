ca5a5e66176c524853fbddef05cfc500e9d0366f
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2025-06-18 17:29:48 +0200

    abc

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -7,5 +7,6 @@ ln -sf $HOME/InitBAS/config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
 ln -sf $HOME/InitBAS/config/r/Renviron $HOME/.Renviron
 ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
-ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
-ln -sf $HOME/InitBAS/config/hyprland/config.ini ~/.config/waypaper/config.ini
+# ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
+# ln -sf $HOME/InitBAS/config/hyprland/config.ini ~/.config/waypaper/config.ini
+# Sjekk
