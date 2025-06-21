3ae63aad5b790d25343a57eeb85e0e0f1fcd9164
jorn.halvorsen <jorn.i.halvorsen@hiof.no>
2025-06-21 22:53:58 +0200

    upd

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -40,13 +40,13 @@ fi
 yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
 
 ### I
-fileh="$HOME/.config/hypr"                                                             
-if [ ! -d "$fileh" ]; then
-  echo "Hyprland is already installed."
-else
-  echo "Hyprland is not installed. Proceeding with the installation..."
-  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
-fi
+#fileh="$HOME/.config/hypr"                                                             
+#if [ ! -d "$fileh" ]; then
+#  echo "Hyprland is already installed."
+#else
+#  echo "Hyprland is not installed. Proceeding with the installation..."
+#  bash <(curl -s "https://gitlab.com/stephan-raabe/hyprland-starter/-/raw/main/setup.sh")
+#fi
 
 ### II
 echo 'oh-my-zsh'
