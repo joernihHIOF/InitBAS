f1429e5014ff0ed8c48ccc6ce63c803bdeaad39a
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-06-23 11:58:44 +0200

    noconf

--- 04_plinst.R
+++ 04_plinst.R
@@ -7,33 +7,6 @@ local({
   # Save the updated repository settings in the options
   options(repos = r)
 })
-<<<<<<< HEAD
-
-# Read in a list of package names from the specified file (one package name per line)
-rpack <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
-# Print out the list of packages to confirm what was read
-
-# Define the library path where packages should be installed; this is constructed using the HOME environment variable and a specific directory structure
-library_path <- paste0(Sys.getenv("HOME"),"/R/x86_64-pc-linux-gnu-library/4.4")
-# Print out the library path for verification
-print(library_path)
-
-# If the library directory does not exist, create it (including any necessary parent directories)
-if (!dir.exists(library_path)) {
-  dir.create(library_path, recursive = TRUE)
-}
-
-# Loop through each package in the list
-for (package in rpack) {
-  # Check if the package is not already installed by looking at the row names of installed.packages()
-  if (!(package %in% rownames(installed.packages()))) {
-    # If the package is not installed, install it from CRAN using the previously set repository
-    install.packages(package)
-  }
-}
-
-# /home/joernih/R/x86_64-pc-linux-gnu-library/4.4
-=======
 rpack <- readLines("~/InitBAS/packages/rpkbase.txt")                        
 # Check if each package is already installed, if not, install it     
 r_version <- paste(R.version$major, R.version$minor, sep = ".")
@@ -49,5 +22,4 @@ for (package in rpack) {
   }                                                                  
 }                                                                    
 # py_install("numba")
->>>>>>> 74a8361866f4925897f441ba0eb8d64eedb01ca0
 
