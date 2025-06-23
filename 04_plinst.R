afcd3fbe4ba23728235490d5717e5019fc22cf91
joernihHIOF <jorn.i.halvorsen@hiof.no>
2025-06-23 11:55:03 +0200

    updrplin

--- 04_plinst.R
+++ 04_plinst.R
@@ -1,21 +1,36 @@
+# Temporarily set the CRAN repository to a specific URL
 local({
+  # Retrieve the current repository settings
   r <- getOption("repos")
+  # Update the CRAN repository to use the specified URL
   r["CRAN"] <- "https://cran.uib.no/"
+  # Save the updated repository settings in the options
   options(repos = r)
 })
-rpack <- readLines("~/InitBAS/packages/rpkbase.txt")                        
-# Check if each package is already installed, if not, install it     
+
+# Read in a list of package names from the specified file (one package name per line)
+rpack <- readLines("~/InitBAS/packages/rpkbase.txt")
+# Print out the list of packages to confirm what was read
+print(rpack)
+
+# Define the library path where packages should be installed; this is constructed using the HOME environment variable and a specific directory structure
 library_path <- paste0(Sys.getenv("HOME"),"/R/x86_64-pc-linux-gnu-library/4.4")
+# Print out the library path for verification
 print(library_path)
-if (!dir.exists(library_path)) {                                                                          
-  dir.create(library_path, recursive = TRUE)                                                              
-}                                                                                                         
-#  'lib = "/usr/lib/R/library"' is not writable
-#for (package in rpack) {                                          
-#  if (!(package %in% rownames(installed.packages()))) {              
-#    install.packages(package)
-#  }                                                                  
-#}                                                                    
-#
-# py_install("numba")
+
+# If the library directory does not exist, create it (including any necessary parent directories)
+if (!dir.exists(library_path)) {
+  dir.create(library_path, recursive = TRUE)
+}
+
+# Loop through each package in the list
+for (package in rpack) {
+  # Check if the package is not already installed by looking at the row names of installed.packages()
+  if (!(package %in% rownames(installed.packages()))) {
+    # If the package is not installed, install it from CRAN using the previously set repository
+    install.packages(package)
+  }
+}
+
+# /home/joernih/R/x86_64-pc-linux-gnu-library/4.4
 
