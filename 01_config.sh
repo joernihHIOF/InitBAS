2af44dffd87ad428a548bfcad6266386142f379d
joernih <jorn.halvorsen@gmail.com>
2025-06-29 23:36:15 +0200

    hyprla

--- 01_config.sh
+++ 01_config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+if [ ! -f "../.environBAS" ]; then
+    cp config/.environBAS ../.environBAS
+fi
 source ~/.environBAS
-# Github configurations
+## Github configurations
 git config --global user.name $NAME
 git config --global user.email $EMAIL
 git config --global core.editor $EDITOR
-
 
