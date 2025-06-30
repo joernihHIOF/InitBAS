3c558de4dd11b380d4f2eced8b93957dfe02e15f
joernih <jorn.halvorsen@gmail.com>
2025-06-30 21:58:02 +0200

    upd

--- 05_plinst.R
+++ 05_plinst.R
@@ -1,4 +1,4 @@
-## Temporarily set the CRAN repository to a specific URL
+# Temporarily set the CRAN repository to a specific URL
 local({
   # Retrieve the current repository settings
   r <- getOption("repos")
@@ -8,21 +8,20 @@ local({
   options(repos = r)
 })
 
-# Fixed?
 rabc <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
-lib_path <- paste0(Sys.getenv("HOME"), "/R/x86_64-pc-linux-gnu-library/")
-if (!dir.exists(lib_path)) {
-    dir.create(lib_path, recursive = TRUE)
-}
-.libPaths(c(lib_path, .libPaths()))
-for (package in rabc) {
-    if (!require(package, character.only = TRUE, quietly = TRUE)) {
-        install.packages(package, lib = lib_path)
-    }
-}
+rpack <- .libPaths()[1]
+lib_path <- paste0(Sys.getenv("HOME"), "/R/x86_64-pc-linux-gnu-library/", rpack)
+if (!dir.exists(lib_path)) {                                                                            dir.create(lib_path, recursive = TRUE)                                                              
+}                                                                                                     
+for (package in rabc) {                                          
+  if ((!package %in% rownames(installed.packages()))) {              
+    print(rpack)
+    install.packages(package)
+    #install.packages("askpass")
+  }                                                                  
+}                                                                    
 
 
-#if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
-#  dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
-#}
-#install.packages("ellmer", lib = Sys.getenv("R_LIBS_USER"))
+reticulate::py_require("numpy")
+reticulate::py_require("sympy")
+reticulate::py_require("pandas")
