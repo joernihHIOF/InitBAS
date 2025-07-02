c01dc722f67aafe4fac39e7151cfc876200f36af
joernih <jorn.halvorsen@gmail.com>
2025-07-02 14:50:46 +0200

    fix

--- 04_plinst.R
+++ 04_plinst.R
@@ -1,21 +1,22 @@
 if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
   dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
 }
-install.packages("ellmer")
 
+rabc <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
+for (pins in rabc) {                                          
+  if ((!package %in% rownames(installed.packages()))) {              
+    print(rpack)
+    install.packages(pins)
+  }                                                                  
+}                                                                    
 
+#install.packages("ellmer")
 #install.packages("ellmer")
 #rpack <- .libPaths()[1]
 #print(rpack)
 #lib_path <- paste0(Sys.getenv("HOME"), "/R/x86_64-pc-linux-gnu-library/", rpack)
 #if (!dir.exists(lib_path)) {                                                                            dir.create(lib_path, recursive = TRUE)                                                              
 #}                                                                                                     
-#for (package in rabc) {                                          
-#  if ((!package %in% rownames(installed.packages()))) {              
-#    print(rpack)
-#    install.packages(package)
-#  }                                                                  
-#}                                                                    
 #
 #reticulate::py_require("numpy")
 #reticulate::py_require("sympy")
@@ -27,7 +28,7 @@ install.packages("ellmer")
 
 
 #> install.packages("ellmer")
-#Warning in install.packages("ellmer") :
+#Warning in inskl.packages("ellmer") :
 #  'lib = "/usr/lib/R/library"' is not writable
 #Would you like to use a personal library instead? (yes/No/cancel) 
 ## Temporarily set the CRAN repository to a specific URL
@@ -40,7 +41,6 @@ install.packages("ellmer")
 #  options(repos = r)
 #})
 #
-#rabc <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
 #rpack <- .libPaths()[1]
 #lib_path <- paste0(Sys.getenv("HOME"), "/R/x86_64-pc-linux-gnu-library/", rpack)
 #if (!dir.exists(lib_path)) {                                                                            dir.create(lib_path, recursive = TRUE)                                                              
