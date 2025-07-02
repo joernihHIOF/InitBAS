238e2917e12f5661cabfb34801fd4f896e207eb2
joernih <jorn.halvorsen@gmail.com>
2025-07-02 19:39:30 +0200

    upd

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -1,3 +1,4 @@
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 [ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim 
 [ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
 [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
@@ -8,3 +9,7 @@ ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
 ln -sf $HOME/InitBAS/config/r/Renviron $HOME/.Renviron
 ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
 ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
+
+#[ ! -d etc/issue ] && touch etc/issue
+#touch: cannot touch 'etc/issue': No such file or directory
+##ln -sf $HOME/InitJIH/config/tty1/issue etc/issue 
