2eb17f36a428e17a9862fb137922591553cff6d7
joernih <jorn.halvorsen@gmail.com>
2025-07-03 15:43:21 +0200

    abc

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -14,5 +14,7 @@ ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper
  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
-# ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
-# ln -sf $HOME/.environBAS $HOME/.Renviron
+ln -sf $HOME/.environBAS $HOME/.Renviron
+ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
+
+
