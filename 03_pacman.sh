f50cc098657aa4d202b5739d94ff6038710536db
joernih <jorn.halvorsen@gmail.com>
2025-07-03 14:12:38 +0200

    comoff

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -9,49 +9,49 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.v
 # ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
 # ln -sf $HOME/.environBAS $HOME/.Renviron
 
-## Yay
-echo 'yay starter'
-if ! command -v yay &> /dev/null
-then
-    echo "yay not found. Installing yay..."
-    
-    # Install dependencies if not already installed
-    sudo pacman -Sy --needed git base-devel
-    
-    # Clone yay repository
-    git clone https://aur.archlinux.org/yay.git /tmp/yay-install
-    
-    # Build and install yay
-    #cd /tmp/yay-install || exit
-    #makepkg -si --noconfirm
-    # Jump to the new directory, then return after install
-    pushd /tmp/yay-install || exit
-    makepkg -si --noconfirm
-    echo "yay has been installed successfully."
-    popd
-    
-    rm -rf /tmp/yay-install
-    echo "updating yay"
-    yay -Syu --noconfirm
-    yay -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
-else
-    echo "updating yay"
-    yay -Syu --noconfirm
-    yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
-fi
-## 
-
-## Zsh
-## Oh-my-zsh
-if ! [ -n "$ZSH" ]
-then
-    echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo "ABC"
-else
-    echo "ZSH already installed"
-fi
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +UpdateRemotePlugins +qall
-
+### Yay
+#echo 'yay starter'
+#if ! command -v yay &> /dev/null
+#then
+#    echo "yay not found. Installing yay..."
+#    
+#    # Install dependencies if not already installed
+#    sudo pacman -Sy --needed git base-devel
+#    
+#    # Clone yay repository
+#    git clone https://aur.archlinux.org/yay.git /tmp/yay-install
+#    
+#    # Build and install yay
+#    #cd /tmp/yay-install || exit
+#    #makepkg -si --noconfirm
+#    # Jump to the new directory, then return after install
+#    pushd /tmp/yay-install || exit
+#    makepkg -si --noconfirm
+#    echo "yay has been installed successfully."
+#    popd
+#    
+#    rm -rf /tmp/yay-install
+#    echo "updating yay"
+#    yay -Syu --noconfirm
+#    yay -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
+#else
+#    echo "updating yay"
+#    yay -Syu --noconfirm
+#    yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
+#fi
+### 
+#
+### Zsh
+### Oh-my-zsh
+#if ! [ -n "$ZSH" ]
+#then
+#    echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
+#    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#    echo "ABC"
+#else
+#    echo "ZSH already installed"
+#fi
+#
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#nvim +UpdateRemotePlugins +qall
+#
--- 05_dsktop.sh
+++ 05_dsktop.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
-
-## Function to clone repository if it doesn't exist
-clone_if_not_exists() {
-    repo_url="$1"
-    target_dir="$2"
-    
-    if [ ! -d "$target_dir" ]; then
-        git clone --depth 1 "$repo_url" "$target_dir"
-    else
-        echo "Directory $target_dir already exists. Skipping clone."
-    fi
-}
-
-# Desktop managers
-## Hyprland
-if ! command -v Hyprland &> /dev/null
-then
-    echo "Hyprland is not installed. Proceeding with the installation..."
-	sudo pacman -S hyprland
-	sudo pacman -S waybar rofi-wayland alacritty dunst dolphin xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpaper hyprlock ttf-font-awesome
-        ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
-        ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
-else
-    echo "Hyprland found"
-fi
-
-
+##!/bin/bash
 #
-## KDE plasma
-sudo pacman -S plasma kde-applications --needed
-sudo pacman -S plasma-nm --needed
-sudo pacman -S plasma-wayland-session --needed
+### Function to clone repository if it doesn't exist
+#clone_if_not_exists() {
+#    repo_url="$1"
+#    target_dir="$2"
+#    
+#    if [ ! -d "$target_dir" ]; then
+#        git clone --depth 1 "$repo_url" "$target_dir"
+#    else
+#        echo "Directory $target_dir already exists. Skipping clone."
+#    fi
+#}
 #
-## Qtile
-sudo pacman -S qtile
-
-
-
+## Desktop managers
+### Hyprland
+#if ! command -v Hyprland &> /dev/null
+#then
+#    echo "Hyprland is not installed. Proceeding with the installation..."
+#	sudo pacman -S hyprland
+#	sudo pacman -S waybar rofi-wayland alacritty dunst dolphin xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpaper hyprlock ttf-font-awesome
+#        ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
+#        ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
+#else
+#    echo "Hyprland found"
+#fi
+#
+#
+##
+### KDE plasma
+#sudo pacman -S plasma kde-applications --needed
+#sudo pacman -S plasma-nm --needed
+#sudo pacman -S plasma-wayland-session --needed
+##
+### Qtile
+#sudo pacman -S qtile
+#
+#
+#
