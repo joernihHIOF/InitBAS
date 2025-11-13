ce1341f7f57424b58e141fd628ace10088e92cfe
joernih <jorn.halvorsen@gmail.com>
2025-11-13 18:18:16 +0100

    upd

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -4,11 +4,24 @@
 [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 # sudo ln -sf $HOME/InitBAS/config/tty1/issue /etc/issue 
 
-# ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
-# ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
-ln -sf $HOME/InitBAS/config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
-ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
-ln -sf $HOME/.environBAS $HOME/.Renviron
+#
+## Hyprland
+ln -sf config/hypr/autostart.conf              $HOME/.config/hypr/autostart.conf 
+ln -sf config/hypr/bindings.conf               $HOME/.config/hypr/bindings.conf  
+ln -sf config/hypr/envs.conf                   $HOME/.config/hypr/envs.conf      
+ln -sf config/hypr/hypridle.conf               $HOME/.config/hypr/hypridle.conf  
+ln -sf config/hypr/hyprland.conf               $HOME/.config/hypr/hyprland.conf  
+ln -sf config/hypr/hyprlock.conf               $HOME/.config/hypr/hyprlock.conf  
+ln -sf config/hypr/hyprsunset.conf             $HOME/.config/hypr/hyprsunset.conff
+ln -sf config/hypr/input.conf                  $HOME/.config/hypr/input.conf     
+ln -sf config/hypr/looknfeel.conf              $HOME/.config/hypr/looknfeel.conf 
+ln -sf config/hypr/monitors.conf               $HOME/.config/hypr/monitors.conf  
+## Neovim
+ln -sf $HOME/InitBAS/config/nvim/init.lua      $HOME/.config/nvim/init.lua
+## Bash
+ln -sf $HOME/InitBAS/config/bash/bashrc        $HOME/.bashrc
+## R
+ln -sf $HOME/InitBAS/config/r/Rprofile         $HOME/.Rprofile
+# ln -sf $HOME/.environBAS                       $HOME/.Renviron
 
 
