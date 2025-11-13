28b5b702cc48dcb3444c202e99569a1d7e55c824
joernih <jorn.halvorsen@gmail.com>
2025-11-13 15:44:21 +0100

    rstart

--- 04_plinst.R
+++ 04_plinst.R
@@ -1,26 +1,26 @@
-#if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
-#  dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
-#}
+if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+  dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
+}
+
+spack <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
+for (pins in spack) {                                          
+  if ((!pins %in% rownames(installed.packages()))) {              
+    print(pins)
+    install.packages(pins)
+  }                                                                  
+}                                                                    
 #
-#spack <- (x <- readLines("~/InitBAS/packages/rpkbase.txt"))[!grepl("^#", x)]
-#for (pins in spack) {                                          
-#  if ((!pins %in% rownames(installed.packages()))) {              
-#    print(pins)
-#    install.packages(pins)
-#  }                                                                  
-#}                                                                    
-
-
-# Temporarily set the CRAN repository to a specific URL
-# local({
-#   # Retrieve the current repository settings
-#   r <- getOption("repos")
-#   # Update the CRAN repository to use the specified URL
-#   r["CRAN"] <- "https://cran.uib.no/"
-#   # Save the updated repository settings in the options
-#   options(repos = r)
-# })
-# 
+#
+## Temporarily set the CRAN repository to a specific URL
+#local({
+#  # Retrieve the current repository settings
+#  r <- getOption("repos")
+#  # Update the CRAN repository to use the specified URL
+#  r["CRAN"] <- "https://cran.uib.no/"
+#  # Save the updated repository settings in the options
+#  options(repos = r)
+#})
+## 
 # rabc <- (x <- readLines("~/InitJIH/packages/rpkbase.txt"))[!grepl("^#", x)]
 # rpack <- .libPaths()[1]
 # lib_path <- paste0(Sys.getenv("HOME"), "/R/x86_64-pc-linux-gnu-library/", rpack)
@@ -34,10 +34,9 @@
 #   }                                                                  
 # }                                                                    
 # 
-# #reticulate::py_require("numpy")
-# #reticulate::py_require("sympy")
-# #reticulate::py_require("pandas")
-# 
+reticulate::py_require("numpy")
+reticulate::py_require("sympy")
+reticulate::py_require("pandas")
 
 
 
