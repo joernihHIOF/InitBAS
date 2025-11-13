13545b0d607eb01bf870f7ad4ad84f2a031f22d4
joernih <jorn.halvorsen@gmail.com>
2025-11-13 18:07:22 +0100

    abc

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -1,19 +1,14 @@
-ln -sf $HOME/InitBAS/config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
-
-# [ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim 
-# [ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
-# [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 # [ ! -d etc/issue ] && sudo touch /etc/issue
+[ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim 
+[ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
+[ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 # sudo ln -sf $HOME/InitBAS/config/tty1/issue /etc/issue 
+
 # ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
 # ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
-# 
-#  ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
-#  ln -sf $HOME/.environBAS $HOME/.Renviron
-#  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# 
-# ln -sf $HOME/.environBAS $HOME/.Renviron
-# ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
+ln -sf $HOME/InitBAS/config/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
+ln -sf $HOME/InitBAS/config/r/Rprofile $HOME/.Rprofile
+ln -sf $HOME/.environBAS $HOME/.Renviron
 
 
