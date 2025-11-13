8ed920eb066f9e370d17b11cfab72cba0f8762aa
joernih <jorn.halvorsen@gmail.com>
2025-11-13 11:36:03 +0100

    neste

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -2,7 +2,7 @@
 ## Pacman
 #echo 'pacman starter'
 sudo pacman -Sy archlinux-keyring --needed
-#sudo pacman -S --needed base-devel --noconfirm
+sudo pacman -S --needed base-devel --noconfirm
 #sudo pacman -Syu --noconfirm
 #sudo pacman -Sy fastfetch --needed
 #sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.txt 2>/dev/null
