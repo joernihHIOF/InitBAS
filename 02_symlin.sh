72d5c6958196d560cd0aedc4d8550d9d157252f4
joernih <jorn.halvorsen@gmail.com>
2025-11-13 11:29:08 +0100

    upd

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -1,10 +1,11 @@
+# ln -sf $HOME/InitBAS/config/nvim/init.lua $HOME/.config/nvim/init.lue
+
 # [ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim 
 # [ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
 # [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 # [ ! -d etc/issue ] && sudo touch /etc/issue
 # sudo ln -sf $HOME/InitBAS/config/tty1/issue /etc/issue 
 # ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
-# ln -sf $HOME/InitBAS/config/nvim/init.vim $HOME/.config/nvim/init.vim
 # ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
 # ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
 # 
