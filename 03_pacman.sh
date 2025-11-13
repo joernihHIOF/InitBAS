daa348de0212f1d65d21b33cd1f1dd9cfaee6a5c
joernih <jorn.halvorsen@gmail.com>
2025-11-13 11:21:10 +0100

    delforomarchy

--- 03_pacman.sh
+++ 03_pacman.sh
@@ -6,51 +6,51 @@
 #sudo pacman -Syu --noconfirm
 #sudo pacman -Sy fastfetch --needed
 #sudo pacman -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/pckfull.txt 2>/dev/null
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-### Yay
-if ! command -v yay &> /dev/null
-then
-    echo "yay not found. Installing yay..."
-    
-    # Install dependencies if not already installed
-    sudo pacman -Sy --needed git base-devel
-    
-    # Clone yay repository
-    git clone https://aur.archlinux.org/yay.git /tmp/yay-install
-    
-    # Build and install yay
-    #cd /tmp/yay-install || exit
-    #makepkg -si --noconfirm
-    # Jump to the new directory, then return after install
-    pushd /tmp/yay-install || exit
-    makepkg -si --noconfirm
-    echo "yay has been installed successfully."
-    popd
-    
-    rm -rf /tmp/yay-install
-    echo "updating yay"
-    yay -Syu --noconfirm
-    yay -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
-else
-    echo "updating yay"
-    yay -Syu --noconfirm
-    yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
-fi
-## 
-
-## Zsh
-## Oh-my-zsh
-if ! [ -n "$ZSH" ]
-then
-    echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo "ABC"
-else
-    echo "ZSH already installed"
-fi
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim +UpdateRemotePlugins +qall
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 #
+#### Yay
+#if ! command -v yay &> /dev/null
+#then
+#    echo "yay not found. Installing yay..."
+#    
+#    # Install dependencies if not already installed
+#    sudo pacman -Sy --needed git base-devel
+#    
+#    # Clone yay repository
+#    git clone https://aur.archlinux.org/yay.git /tmp/yay-install
+#    
+#    # Build and install yay
+#    #cd /tmp/yay-install || exit
+#    #makepkg -si --noconfirm
+#    # Jump to the new directory, then return after install
+#    pushd /tmp/yay-install || exit
+#    makepkg -si --noconfirm
+#    echo "yay has been installed successfully."
+#    popd
+#    
+#    rm -rf /tmp/yay-install
+#    echo "updating yay"
+#    yay -Syu --noconfirm
+#    yay -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
+#else
+#    echo "updating yay"
+#    yay -Syu --noconfirm
+#    yay         -S --noconfirm --needed --noconfirm - < ~/InitBAS/packages/yayfull.txt 2>/dev/null
+#fi
+### 
+#
+### Zsh
+### Oh-my-zsh
+#if ! [ -n "$ZSH" ]
+#then
+#    echo "$PROGRAM_NAME is not installed. Proceeding with the installation..."
+#    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#    echo "ABC"
+#else
+#    echo "ZSH already installed"
+#fi
+#
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#
+#nvim +UpdateRemotePlugins +qall
+##
