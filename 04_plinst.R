50bc9108220e777b4b779236b735a434c8b9ca94
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2025-11-13 23:10:35 +0100

    abc

--- 04_plinst.R
+++ 04_plinst.R
@@ -1,4 +1,4 @@
-1if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
+if (!dir.exists(Sys.getenv("R_LIBS_USER"))) {
   dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
 }
 
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -33,41 +33,11 @@ vim.keymap.set("n", "cp", ":let @+ = expand('%:p')<CR>")
 vim.keymap.set("n", "cp1", ":let @+ = expand('%')<CR>")
 
 vim.keymap.set(
-  "n",
-  "<leader>t",
-  ":terminal ++cwd=%:p:h<CR>",
-  { desc = "Open terminal in current file's project folder" }
+	"n",
+	"<leader>t",
+	":terminal ++cwd=%:p:h<CR>",
+	{ desc = "Open terminal in current file's project folder" }
 )
--- Easy window navigation (non-recursive)
--- vim.keymap.set("n", "<A-h>", "<C-w>h")
--- vim.keymap.set("n", "<A-j>", "<C-w>j")
--- vim.keymap.set("n", "<A-k>", "<C-w>k")
--- vim.keymap.set("n", "<A-l>", "<C-w>l")
--- vim.keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h")
--- vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j")
--- vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k")
--- vim.keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l")
-
--- -- Buffer
--- vim.keymap.set("n", "bn", ":buffer ", opts_r)
--- vim.keymap.set("n", "ba", ":b#<CR>", opts_r)
--- vim.keymap.set("n", "bl", ":buffers<CR>", opts_r)
--- vim.keymap.set("n", "b1", ":buffer 1<CR>", opts_r)
--- vim.keymap.set("n", "b2", ":buffer 2<CR>", opts_r)
--- vim.keymap.set("n", "b3", ":buffer 3<CR>", opts_r)
--- vim.keymap.set("n", "b4", ":buffer 4<CR>", opts_r)
--- vim.keymap.set("n", "b5", ":buffer 5<CR>", opts_r)
--- vim.keymap.set("n", "b6", ":buffer 6<CR>", opts_r)
--- vim.keymap.set("n", "b7", ":buffer 7<CR>", opts_r)
--- vim.keymap.set("n", "b8", ":buffer 8<CR>", opts_r)
--- vim.keymap.set("n", "b9", ":buffer 9<CR>", opts_r)
---
---
--- -- Source and copy path
--- vim.keymap.set("n", "cs", ":source ~/InitBAS/config/nvim/init.vim<CR>", opts_r)
--- vim.keymap.set("n", "cp", ":let @+ = expand('%:p')<CR>", opts_r)
--- vim.keymap.set("n", "cp1", ":let @+ = expand('%')<CR>", opts_r)
---
 -- -- Edit configuration files
 -- vim.keymap.set("n", "ser1", ":e ~/InitBAS/config/r/Rprofile<CR>", opts_r)
 -- vim.keymap.set("n", "ser2", ":e ~/InitBAS/config/r/Renviron<CR>", opts_r)
