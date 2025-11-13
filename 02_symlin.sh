0246a3eaf4839e48c073aa8c13305a213b8d678c
Jørn I. Halvorsen <jorn.halvorsen@gmail.com>
2025-11-13 14:03:38 +0100

    added

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -1,11 +1,11 @@
 ln -sf $HOME/InitBAS/config/nvim/init.lua $HOME/.config/nvim/init.lua
+# ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
 
 # [ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim 
 # [ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
 # [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 # [ ! -d etc/issue ] && sudo touch /etc/issue
 # sudo ln -sf $HOME/InitBAS/config/tty1/issue /etc/issue 
-# ln -sf $HOME/InitBAS/config/bash/bashrc $HOME/.bashrc
 # ln -sf $HOME/InitBAS/config/hyprland/hyprland.conf  $HOME/.config/hypr/hyprland.conf
 # ln -sf $HOME/InitBAS/config/hyprland/hyprpaper.conf $HOME/.config/hypr/hyprpaper.conf
 # 
