ee01d6db9313b5b3de2e289fb94e8331411e9121
joernih <jorn.halvorsen@gmail.com>
2025-11-14 21:49:52 +0100

    ab

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+require("lazy").setup({
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+})
+
 -- abc
 vim.opt.swapfile = false
 vim.keymap.set("n", "c1", ":split<CR>", { noremap = true, silent = true })
@@ -28,26 +35,15 @@ vim.keymap.set("n", "cb", ":!")
 vim.keymap.set("n", "ce", ":e ")
 vim.keymap.set("n", "co", ":")
 vim.keymap.set("n", "fin", "/")
-
 vim.keymap.set("n", "cp", ":let @+ = expand('%:p')<CR>")
 vim.keymap.set("n", "cp1", ":let @+ = expand('%')<CR>")
 
-vim.keymap.set(
-	"n",
-	"<leader>t",
-	":terminal ++cwd=%:p:h<CR>",
-	{ desc = "Open terminal in current file's project folder" }
-)
--- -- Edit configuration files
--- vim.keymap.set("n", "ser1", ":e ~/InitBAS/config/r/Rprofile<CR>", opts_r)
--- vim.keymap.set("n", "ser2", ":e ~/InitBAS/config/r/Renviron<CR>", opts_r)
--- vim.keymap.set("n", "sea",  ":e ~/InitBAS/alias.sh<CR>", opts_r)
--- vim.keymap.set("n", "sef",  ":e ~/InitBAS/functions.sh<CR>", opts_r)
--- vim.keymap.set("n", "sev",  ":e ~/InitBAS/config/nvim/init.vim<CR>", opts_r)
--- vim.keymap.set("n", "seb",  ":e ~/InitBAS/config/bash/bashrc<CR>", opts_r)
--- vim.keymap.set("n", "sez",  ":e ~/InitBAS/config/zsh/zshrc<CR>", opts_r)
--- vim.keymap.set("n", "seq",  ":e ~/InitBAS/config/qtile/config.py<CR>", opts_r)
--- vim.keymap.set("n", "sek",  ":e ~/InitBAS/kitty/kitty.conf<CR>", opts_r)
--- vim.keymap.set("n", "seh",  ":e ~/InitBAS/config/hyprland/hyprland.conf<CR>", opts_r)
--- vim.keymap.set("n", "sec",  ":e ~/InitBAS/README.md<CR>", opts_r)
--- vim.keymap.set("n", "kmp",  ":e ~/InitBAS/config/nvim/keymapping.vim<CR>", opts_r)
+-- vim.g.maplocalleader = "," -- change ',' to your desired key
+--
+-- vim.api.nvim_buf_set_keymap(
+--   0,
+--   "n",
+--   "<LocalLeader>L",
+--   "<Cmd>lua require('r.send').cmd('devtools::load_all()')<CR>",
+--   { desc = "R: devtools::load_all()", noremap = true, silent = true }
+-- )
