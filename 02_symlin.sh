7a06fd64f33592ec4155e2d6801b1e798c043baf
joernih <jorn.halvorsen@gmail.com>
2025-11-20 21:26:46 +0100

    abcd

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -6,20 +6,24 @@
 ## Hyprland
 cp -f config/hypr/monitors.conf               $HOME/.config/hypr/monitors.conf  
 cp -f config/hypr/bindings.conf               $HOME/.config/hypr/bindings.conf  
-# ln -sf config/hypr/autostart.conf              $HOME/.config/hypr/autostart.conf 
-# ln -sf config/hypr/envs.conf                   $HOME/.config/hypr/envs.conf      
-# ln -sf config/hypr/hypridle.conf               $HOME/.config/hypr/hypridle.conf  
-# ln -sf config/hypr/hyprland.conf               $HOME/.config/hypr/hyprland.conf  
-# ln -sf config/hypr/hyprlock.conf               $HOME/.config/hypr/hyprlock.conf  
-# ln -sf config/hypr/hyprsunset.conf             $HOME/.config/hypr/hyprsunset.conff
-# ln -sf config/hypr/input.conf                  $HOME/.config/hypr/input.conf     
-# ln -sf config/hypr/looknfeel.conf              $HOME/.config/hypr/looknfeel.conf 
-## Neovim
-ln -sf $HOME/InitBAS/config/nvim/vim/init.vim  $HOME/.config/nvim/vim/init.vim
-ln -sf $HOME/InitBAS/config/nvim/init.lua      $HOME/.config/nvim/init.lua
-## Bash
-ln -sf $HOME/InitBAS/config/bash/bashrc         $HOME/.bashrc
-## R
-ln -sf $HOME/InitBAS/config/r/Rprofile          $HOME/.Rprofile
-
+# cp -f config/hypr/autostart.conf              $HOME/.config/hypr/autostart.conf 
+# cp -f config/hypr/envs.conf                   $HOME/.config/hypr/envs.conf      
+# cp -f config/hypr/hypridle.conf               $HOME/.config/hypr/hypridle.conf  
+# cp -f config/hypr/hyprland.conf               $HOME/.config/hypr/hyprland.conf  
+# cp -f config/hypr/hyprlock.conf               $HOME/.config/hypr/hyprlock.conf  
+# cp -f config/hypr/hyprsunset.conf             $HOME/.config/hypr/hyprsunset.conff
+# cp -f config/hypr/input.conf                  $HOME/.config/hypr/input.conf     
+# cp -f config/hypr/looknfeel.conf              $HOME/.config/hypr/looknfeel.conf 
+## Vim
+cp $HOME/InitBAS/config/nvim/vim/init.vim  $HOME/.config/nvim/vim/init.vim
+cp $HOME/InitBAS/config/nvim/vim/keymapping.vim  $HOME/.config/nvim/vim/keymapping.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+## Neovim
+cp $HOME/InitBAS/config/nvim/init.lua      $HOME/.config/nvim/init.lua
+cp -r $HOME/InitBAS/config/nvim/lua/config    $HOME/.config/nvim/lua/config
+cp -r $HOME/InitBAS/config/nvim/lua/plugins   $HOME/.config/nvim/lua/plugins
+### Bash
+cp $HOME/InitBAS/config/bash/bashrc         $HOME/.bashrc
+### R
+cp $HOME/InitBAS/config/r/Rprofile          $HOME/.Rprofile
+#cp $HOME/.environBAS                        $HOME/.Renviron
