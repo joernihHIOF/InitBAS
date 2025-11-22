89a10ba13f40953ddef97abdba8f821b8036821b
joernih <jorn.halvorsen@gmail.com>
2025-11-22 11:50:38 +0100

    upd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 <<<<<<< HEAD
+<<<<<<< HEAD
 require("nvim-python-repl").setup({
   spawn_command = {
     python = "python", -- use "python" or "python3" instead of "ipython"
   },
 })
+=======
+>>>>>>> 5c174a0 (updcp)
 =======
 >>>>>>> 5c174a0 (updcp)
