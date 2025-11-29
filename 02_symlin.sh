3580a15a51d780fc4015e62f74a073794e7a9e2c
joernih <jorn.halvorsen@gmail.com>
2025-11-29 11:12:16 +0100

    initbas

--- 02_symlin.sh
+++ 02_symlin.sh
@@ -3,25 +3,24 @@
 [ ! -d $HOME/.config/hypr ] && mkdir -p $HOME/.config/hypr 
 [ ! -d $HOME/.config/qtile ] && mkdir -p $HOME/.config/qtile 
 
-## Neovim
-cp $HOME/InitBAS/config/nvim/init.lua      $HOME/.config/nvim/init.lua
-cp -r $HOME/InitBAS/config/nvim/lua/config    $HOME/.config/nvim/lua
-cp -r $HOME/InitBAS/config/nvim/lua/plugins   $HOME/.config/nvim/lua
-# ln -sf config/hypr/autostart.conf              $HOME/.config/hypr/autostart.conf 
-# ln -sf config/hypr/envs.conf                   $HOME/.config/hypr/envs.conf      
-# ln -sf config/hypr/hypridle.conf               $HOME/.config/hypr/hypridle.conf  
-# ln -sf config/hypr/hyprland.conf               $HOME/.config/hypr/hyprland.conf  
-# ln -sf config/hypr/hyprlock.conf               $HOME/.config/hypr/hyprlock.conf  
-# ln -sf config/hypr/hyprsunset.conf             $HOME/.config/hypr/hyprsunset.conff
-# ln -sf config/hypr/input.conf                  $HOME/.config/hypr/input.conf     
-# ln -sf config/hypr/looknfeel.conf              $HOME/.config/hypr/looknfeel.conf 
+## Hyprland
+cp -f config/hypr/autostart.conf              $HOME/.config/hypr/autostart.conf 
+cp -f config/hypr/bindings.conf               $HOME/.config/hypr/bindings.conf  
+# cp -f config/hypr/monitors.conf               $HOME/.config/hypr/monitors.conf  
+# cp -f config/hypr/envs.conf                   $HOME/.config/hypr/envs.conf      
+# cp -f config/hypr/hypridle.conf               $HOME/.config/hypr/hypridle.conf  
+# cp -f config/hypr/hyprland.conf               $HOME/.config/hypr/hyprland.conf  
+# cp -f config/hypr/hyprlock.conf               $HOME/.config/hypr/hyprlock.conf  
+# cp -f config/hypr/hyprsunset.conf             $HOME/.config/hypr/hyprsunset.conff
+# cp -f config/hypr/input.conf                  $HOME/.config/hypr/input.conf     
+# cp -f config/hypr/looknfeel.conf              $HOME/.config/hypr/looknfeel.conf 
 ## Vim
 cp $HOME/InitBAS/config/nvim/vim/init.vim  $HOME/.config/nvim/vim/init.vim
 cp $HOME/InitBAS/config/nvim/vim/keymapping.vim  $HOME/.config/nvim/vim/keymapping.vim
 ## Neovim
 cp $HOME/InitBAS/config/nvim/init.lua      $HOME/.config/nvim/init.lua
-cp -r $HOME/InitBAS/config/nvim/lua/config    $HOME/.config/nvim/lua/config
-cp -r $HOME/InitBAS/config/nvim/lua/plugins   $HOME/.config/nvim/lua/plugins
+cp -r $HOME/InitBAS/config/nvim/lua/config    $HOME/.config/nvim/lua
+cp -r $HOME/InitBAS/config/nvim/lua/plugins   $HOME/.config/nvim/lua
 ### Bash
 cp $HOME/InitBAS/config/bash/bashrc         $HOME/.bashrc
 ### R
